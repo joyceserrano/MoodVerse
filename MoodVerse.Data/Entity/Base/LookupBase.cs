8d9b20bdab48d5e1af5fc5e737aa7f4928d954e4
mid-nights <154849940+mid-nights@users.noreply.github.com>
2024-11-15 13:06:35 +0800

    Renamed the folder of Base Classes

---
+++ MoodVerse.Data/Entity/Base/LookupBase.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodVerse.Data.Entity.Initial
+{
+    public class LookupBase
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
