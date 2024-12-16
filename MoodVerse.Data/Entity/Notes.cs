576e95eb8fe649bbfbb5e4552538bca576abd4b5
mid-nights <154849940+mid-nights@users.noreply.github.com>
2024-12-16 17:31:58 +0800

    Migration - Added Notes Table

---
+++ MoodVerse.Data/Entity/Notes.cs
@@ -0,0 +1,12 @@
+﻿using MoodVerse.Data.Entity.Initial;
+using MoodVerse.Data.Entity.Lookups;
+
+namespace MoodVerse.Data.Entity
+{
+    public class Notes : Logged
+    {
+        public string Text { get; set; }
+        public Guid PrimaryEmotionTypeId { get; set; }
+        public PrimaryEmotionType PrimaryEmotionType { get; set; }
+    }
+}
---
+++ MoodVerse.Data/Mapping/NotesMap.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoodVerse.Data.Entity;
+
+namespace MoodVerse.Data.Mapping
+{
+    public class NotesMap
+    {
+        public NotesMap(ModelBuilder modelBuilder) {
+
+            var entityBuilder = modelBuilder.Entity<Notes>();
+
+            entityBuilder.Property(n => n.Text).IsRequired();
+
+            entityBuilder.Property(n => n.PrimaryEmotionTypeId).IsRequired();
+
+            entityBuilder.
+                 HasOne(n => n.PrimaryEmotionType)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entityBuilder.
+                 HasOne(n => n.Creator)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entityBuilder.
+                 HasOne(n => n.Modifier)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
