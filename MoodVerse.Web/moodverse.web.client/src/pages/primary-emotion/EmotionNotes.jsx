82a7050dd25e3e1c9fa26f96972f1e7ce4e07108
mid-nights <154849940+mid-nights@users.noreply.github.com>
2024-12-16 16:39:48 +0800

    [Issue #1] - Added animations for the Emotions

--- MoodVerse.Web/moodverse.web.client/src/pages/primary-emotion/EmotionNotes.jsx
+++ MoodVerse.Web/moodverse.web.client/src/pages/primary-emotion/EmotionNotes.jsx
@@ -1,19 +1,43 @@
-import classes from "./EmotionNotes.module.scss";
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
 import PropTypes from 'prop-types';
+import { useEffect, useRef } from "react";
+import classes from "./EmotionNotes.module.scss";
 
-const EmotionNotes = ({ name }) => {
+gsap.registerPlugin(TextPlugin);
+
+const EmotionNotes = ({ name, onChange }) => {
+    const textRef = useRef(null); 
+
+    useEffect(() => {
+        const textElement = textRef.current;
+        const textContent = textElement.textContent; 
+        textElement.textContent = ""; 
+
+        gsap.to(textElement, {
+            duration: textContent.length * 0.1, 
+            text: { value: textContent }, 
+            ease: `steps(${textContent.length})`
+        });
+    }, []);
+
     return (
         <div className={classes.emotions_notes}>
-            <h1 className={`${classes.line_1} ${classes.anim_typewriter} ${classes.header_text}`}>
-                  I feel <span className={classes.header_name}>{name}</span>...
+            <h1 className={`${classes.header_text}`}>
+                <span ref={textRef} className={classes.gradient_text}>
+                    I feel <span>{name}</span> ...
+                </span>
+                <span className={classes.cursor}> </span>
             </h1>
-            <textarea></textarea>
+            <textarea onChange={onChange}></textarea>
         </div>
     );
 };
 
 EmotionNotes.propTypes = {
     name: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    onChange: PropTypes.func
 };
 
 export default EmotionNotes;
--- MoodVerse.Web/moodverse.web.client/src/pages/primary-emotion/PrimaryEmotionPage.jsx
+++ MoodVerse.Web/moodverse.web.client/src/pages/primary-emotion/PrimaryEmotionPage.jsx
@@ -4,28 +4,67 @@ import { useQuery } from '@tanstack/react-query';
 import EmotionNotes from './EmotionNotes';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { useRef, useState } from "react";
+import { gsap } from "gsap";
+import { useInput } from "../../hook/useInput";
 
 const PrimaryEmotionPage = () => {
+    const [selectedEmotion, setSelectedEmotion] = useState(null);
+
+    const containerRefs = useRef([]);
+
     const { data: emotions } = useQuery({
         queryKey: ['primary-emotions'],
         queryFn: httpRequest.Lookups.getPrimaryEmotions,
     });
 
+    const {
+        value: noteValue,
+        onChange: onNoteChange,
+    } = useInput();
+
+    const handleClick = (index) => {
+        const el = containerRefs.current[index];
+        setSelectedEmotion(index);
+
+        gsap.set(el, {
+            position: "fixed",
+            transform: "none" 
+        });
+
+        gsap.to(el, {
+            top: "20%",
+            left: "20%",
+            duration: 0.8,
+            ease: "power1.inOut",
+            onStart: () => {
+                containerRefs.current.forEach((otherEl, i) => {
+                    if (i !== index) {
+                        gsap.to(otherEl, { autoAlpha: 0, duration: 0.3 });
+                    }
+                });
+            }
+        });
+    };
+
     return (
         <div className={classes.primary_emotions_page}>
             <div className={classes.emotions}>
-                {emotions?.map((e) => (
-                    <div key={e.id} className={classes.container}>
-                        <div className={`${classes.circle} ${classes[e.name.toLowerCase()]}`}>{e.name}</div>
-                    </div>
-                ))}
+                    {emotions?.map((e, index) => (
+                        <div key={e.id} className={classes.container} ref={(el) => (containerRefs.current[index] = el)}>
+                            <div className={`${classes.circle_container}`}>
+                                <div className={`${classes.circle} ${classes[e.name.toLowerCase()]}`}
+                                    onClick={() => handleClick(index, e.name)}>{e.name}</div>
+                            </div>
+                        </div>
+                    ))}
             </div>
-            {emotions && <EmotionNotes name="veryyy Angry" />}
-            <div className={classes.submit_container} >
+            {selectedEmotion && <EmotionNotes name={emotions[selectedEmotion].name} onChange={onNoteChange} />}
+            {selectedEmotion && <div className={classes.submit_container} >
                 <button className={classes.submit_btn}>
                     <FontAwesomeIcon icon={faArrowRight} />
                 </button>
-            </div>
+            </div>}
         </div>
     );
 };
