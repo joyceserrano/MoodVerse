7abdb2d2269538a87cceb75a9ecc3480a0ef2406
mid-nights <154849940+mid-nights@users.noreply.github.com>
2024-12-20 20:16:02 +0800

    Added scss as the preprocessor options for css on vite.config.js

--- MoodVerse.Web/moodverse.web.client/vite.config.js
+++ MoodVerse.Web/moodverse.web.client/vite.config.js
@@ -36,6 +36,13 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
+    css: {
+        preprocessorOptions: {
+          scss: {
+            api: 'modern-compiler' 
+          }
+        }
+      },
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
